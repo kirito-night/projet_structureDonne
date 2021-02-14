0480d963aa6e774aa5b5d57194d625bd3b5c15a7
kirito-night <zengfx4869@gmail.com>
2021-02-14 11:01:10 +0100

    corrige sum_carre_2

--- tme1-2/parti2/tab1.c
+++ tme1-2/parti2/tab1.c
@@ -48,12 +48,13 @@ int sum_carre_1(int *tab, int n){
 
 
 int sum_carre_2(int *tab, int n){
-    int i; 
-    int sum = 0 ;
+    int res = 0 , res_carre = 0;
+    int i;
     for (i = 0 ; i < n; i++){
-        sum+= tab[i];
+        res+= tab[i];
+        res_carre+= tab[i] * tab[i];
     }
-    return sum* sum;
+    return 2* (n * res_carre - res*res);
 }
 
 
