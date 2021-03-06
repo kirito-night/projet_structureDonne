3ff2e28963d7c42762db08dbe9ab58548977d9f7
Raphaël Françoise <raph.francoise@orange.fr>
2021-03-06 19:56:45 +0100

    Update

--- tme3-4/comparaisonH.c
+++ tme3-4/comparaisonH.c
@@ -17,12 +17,17 @@ int main(int argc, char* argv[]){
     printf("Combien de lignes du fichier voulez vous lire ?\n");
     scanf("%d",&nb_lignes);
 
+
+    FILE *f = fopen("sortie_vitesse.txt", "w");   /* Ouverture du fichier pour gnuplot */
+
     clock_t temps_initial; 
     clock_t temps_final;
     double temps_cpu;
     BiblioH* b_hachage = charger_n_entrees_h(nomfic, nb_lignes);
 
-    // Recherche avec le numero du livre.
+    
+    /* Recherche avec le numero du livre. */
+
     // int n; 
     // for(n = 0 ; n <  nb_lignes; n++){
     //     temps_initial = clock();
@@ -32,7 +37,9 @@ int main(int argc, char* argv[]){
     //     printf("Temps pour rechercher livre n°%d avec une table de hachage : %.1f\n",n ,temps_cpu);
     // }
 
-    // Recherche à partir du nom d'un auteur 
+    
+    /* Recherche à partir du nom d'un auteur */
+    
     // char* auteur = (char*) malloc(sizeof(char) * 50);
     // printf("Veuillez saisir l'auteur du livre que vous recherchez\n");
     // scanf("%s",auteur);
@@ -42,15 +49,19 @@ int main(int argc, char* argv[]){
     // temps_cpu = ((double)(temps_final -temps_initial));
     // printf("Temps pour rechercher le livre écrit par %s avec une table de hachage : %.1f\n", auteur,temps_cpu);
 
-    // Recherche à partir du nom du livre
+    
+    /* Recherche à partir du nom du livre */
+
     char* titre = (char*) malloc(sizeof(char) * 50);
-    printf("Veuillez saisir l'auteur du livre que vous recherchez\n");
+    printf("Veuillez saisir le titre du livre que vous recherchez\n");
     scanf("%s",titre);
     temps_initial = clock();
     recherche_ouvrage_titre(b_hachage, titre);
     temps_final = clock();
     temps_cpu = ((double)(temps_final -temps_initial));
     printf("Temps pour rechercher le livre %s avec une table de hachage : %.1f\n", titre,temps_cpu);
+
+    fclose(f);   /* Fermeture du fichier pour gnuplot */
 
     return 0;
 }
