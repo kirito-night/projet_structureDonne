138a84a4768b1a3ff4486048248929f2622a1bbb
Raphaël Françoise <raph.francoise@orange.fr>
2021-03-06 19:29:30 +0100

    Update

--- tme3-4/comparaisonLC.c
+++ tme3-4/comparaisonLC.c
@@ -1,10 +1,10 @@
-#include "biblioH.h"
-#include "entreeSortieH.h"
+#include "biblioLC.h"
+#include "entreeSortieLC.h"
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Pour obtenir le temps de calcul avec la table de hachage.
+// Pour obtenir le temps de calcul avec une liste chainée.
 
 int main(int argc, char* argv[]){
 
@@ -20,16 +20,16 @@ int main(int argc, char* argv[]){
     clock_t temps_initial; 
     clock_t temps_final;
     double temps_cpu;
-    BiblioH* b_hachage = charger_n_entrees_h(nomfic, nb_lignes);
+    Biblio* b_LC = charger_n_entrees(nomfic, nb_lignes);
 
-    // Recherche avec le numero du livre.
     // int n; 
+    // Recherche avec le numero du livre.
     // for(n = 0 ; n <  nb_lignes; n++){
     //     temps_initial = clock();
-    //     recherche_ouvrage_num(b_hachage, n);
+    //     recherche_ouvrage_num(b_LC, n);
     //     temps_final = clock();
     //     temps_cpu = ((double)(temps_final -temps_initial));
-    //     printf("Temps pour rechercher livre n°%d avec une table de hachage : %.1f\n",n ,temps_cpu);
+    //     printf("Temps pour rechercher le livre n°%d avec une liste chainée : %.1f\n",n ,temps_cpu);
     // }
 
     // Recherche à partir du nom d'un auteur 
@@ -37,20 +37,20 @@ int main(int argc, char* argv[]){
     // printf("Veuillez saisir l'auteur du livre que vous recherchez\n");
     // scanf("%s",auteur);
     // temps_initial = clock();
-    // recherche_ouvrage_auteur(b_hachage, auteur);
+    // recherche_ouvrage_auteur(b_LC, auteur);
     // temps_final = clock();
     // temps_cpu = ((double)(temps_final -temps_initial));
-    // printf("Temps pour rechercher le livre écrit par %s avec une table de hachage : %.1f\n", auteur,temps_cpu);
+    // printf("Temps pour rechercher le livre écrit par %s avec une liste chainée : %.1f\n", auteur,temps_cpu);
 
     // Recherche à partir du nom du livre
     char* titre = (char*) malloc(sizeof(char) * 50);
     printf("Veuillez saisir l'auteur du livre que vous recherchez\n");
     scanf("%s",titre);
     temps_initial = clock();
-    recherche_ouvrage_titre(b_hachage, titre);
+    recherche_ouvrage_titre(b_LC, titre);
     temps_final = clock();
     temps_cpu = ((double)(temps_final -temps_initial));
-    printf("Temps pour rechercher le livre %s avec une table de hachage : %.1f\n", titre,temps_cpu);
+    printf("Temps pour rechercher le livre %s avec liste chainée : %.1f\n", titre,temps_cpu);
 
     return 0;
 }
