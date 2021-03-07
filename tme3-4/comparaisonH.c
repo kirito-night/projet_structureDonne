0c72b743da58c5eb81f7b9498bc9fd735919940a
kirito-night <zengfx4869@gmail.com>
2021-03-07 23:07:36 +0100

    les graphes

--- tme3-4/comparaisonH.c
+++ tme3-4/comparaisonH.c
@@ -3,19 +3,20 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NB 100
+#include <string.h>
+#define NB 1000
 
 // Pour obtenir le temps de calcul avec la table de hachage.
 
 int main(int argc, char* argv[]){
 
-    if(argc != 2){
+    /*if(argc != 2){
         printf("veuilez entrer le nom du fichier à lire.\n");
         return 0;
     }
-    char *nomfic = argv[1];
+    char *nomfic = argv[1];*/
 
-    FILE *f = fopen("sortie_vitesse_hachage.txt", "w");  // Ouverture du fichier pour gnuplot 
+    FILE *f = fopen("sortie_vitesse.txt", "w");  // Ouverture du fichier pour gnuplot 
     if(f == NULL){
         printf("erreur d'ouverture de fichier");
         exit(1);
@@ -29,23 +30,33 @@ int main(int argc, char* argv[]){
         clock_t temps_final;
         double temps_cpu;
 
-        b_hachage = charger_n_entrees_h(nomfic, n);
+        b_hachage = charger_n_entrees_h("GdeBiblio.txt", n);
 
         temps_initial = clock();
         // Recherche avec le numero du livre.
-        recherche_ouvrage_num(b_hachage, n);
+
+        /*
+        recherche_ouvrage_num(b_hachage, -1);
+        //recherche_ouvrage_num(b_hachage, n);
+        */
+
 
         // Recherche à partir du nom d'un auteur
+        
+        /*
         char* auteur = (char*) malloc(sizeof(char)*50);
-        strcpy(auteur,"tuomak");
+        strcpy(auteur,"tuomakxddd");
         recherche_ouvrage_auteur(b_hachage, auteur);
-
+        */
         // Recherche à partir du nom du livre
+        
+        
         char* titre = (char*) malloc(sizeof(char)*50);
         strcpy(titre,"PDHPXJPZTUGNNRLX");
         recherche_ouvrage_titre(b_hachage, titre);
+        
         temps_final = clock();
-
+        
         temps_cpu = ((double)(temps_final -temps_initial));
 
         fprintf(f,"%d  %f\n", n, temps_cpu);
--- tme3-4/comparaisonLC.c
+++ tme3-4/comparaisonLC.c
@@ -4,19 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define NB 100000
+#define NB 1000
 
 // Pour obtenir le temps de calcul avec une liste chainée.
 
 int main(int argc, char* argv[]){
 
-    if(argc != 2){
+    /*if(argc != 2){
         printf("veuilez entrer le nom du fichier à lire.\n");
         return 0;
     }
-    char *nomfic = argv[1];
+    char *nomfic = argv[1];*/
 
-    FILE *f = fopen("sortie_vitesse_liste_chainee.txt", "w");  // Ouverture du fichier pour gnuplot 
+    FILE *f = fopen("sortie_vitesse.txt", "w");  // Ouverture du fichier pour gnuplot 
     if(f == NULL){
         printf("erreur d'ouverture de fichier");
         exit(1);
@@ -30,24 +30,36 @@ int main(int argc, char* argv[]){
         clock_t temps_final;
         double temps_cpu;
 
-        b_LC = charger_n_entrees(nomfic, n);
+        b_LC = charger_n_entrees("GdeBiblio.txt", n);
 
         temps_initial = clock();
         // Recherche avec le numero du livre.
-        recherche_ouvrage_num(b_LC, n);
-        recherche_ouvrage_num(b_LC, -1);// le cas ou l'element chercher non dedans  
+
+
+
+        //recherche_ouvrage_num(b_LC, -1);
+        //recherche_ouvrage_num(b_LC, -1);// le cas ou l'element chercher non dedans  
+
+
+
         // Recherche à partir du nom d'un auteur
+
+
         /*
         char* auteur = (char*) malloc(sizeof(char)*50);
-        strcpy(auteur,"tuomak");
+        strcpy(auteur,"tuomakmdrmdr");
         recherche_ouvrage_auteur(b_LC, auteur);
         */
+
+
+
         // Recherche à partir du nom du livre
-        /*
+
+        
         char* titre = (char*) malloc(sizeof(char)*50);
         strcpy(titre,"PDHPXJPZTUGNNRLX");
         recherche_ouvrage_titre(b_LC, titre);
-        */
+        
 
         temps_final = clock();
 
